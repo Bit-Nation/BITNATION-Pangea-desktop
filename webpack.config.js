cea9b69157f82fdb94751285b7db614fda6ee3aa
Hoang Hung <hth321@gmail.com>
2018-12-14 14:50:24 +0700

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 var path = require("path");
 var config = {
-  entry: ["./src/App/index.tsx"],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? "warning" : false
+    },
+    entry: ["./src/App/index.tsx"],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.ts|\.tsx$/,
-        loader: "awesome-typescript-loader",
-        exclude: /node_modules/,
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-        exclude: [/node_modules/]
-      }
-    ]
-  },
+    module: {
+        rules: [
+            {
+                test: /\.ts|\.tsx$/,
+                loader: "awesome-typescript-loader",
+                exclude: /node_modules/,
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+                exclude: [/node_modules/]
+            }
+        ]
+    },
 
-  watch: true,
+    watch: true,
 };
 
 module.exports = config;
